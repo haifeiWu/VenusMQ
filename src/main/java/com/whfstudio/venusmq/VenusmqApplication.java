907dddec6400f9a81a75660158a3b170c3fa013a
吴海飞 <haifei.wu@chelaile.net.cn>
2019-08-02 17:54:53 +0800

    update

---
+++ src/main/java/com/whfstudio/venusmq/VenusmqApplication.java
@@ -0,0 +1,13 @@
+package com.whfstudio.venusmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenusmqApplication.class, args);
+	}
+
+}
